6c5693bfb9e663c0b17a30476b1c239cbd53aee6
Gabriel Oliveira <gabrieloliveira.gos04@gmail.com>
2022-07-11 17:51:23 -0300

    Navbar updated

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function searchShow() {
+    var x = document.getElementById("nav-search");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
